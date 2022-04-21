8745ebd07be5dbf73c64d1ed884b011c6f7fb679
Mohammed Alsaleh <Malsalehh0@gmail.com>
2022-04-21 07:31:21 -0700

    Initial Commit

---
+++ FReaction/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FReaction
+//
+//  Created by Mohammed Alsaleh on 20/09/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FReaction/FReactionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FReactionApp.swift
+//  FReaction
+//
+//  Created by Mohammed Alsaleh on 20/09/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct FReactionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
